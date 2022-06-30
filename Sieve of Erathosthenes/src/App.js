e5166d62d85b9f767c5665dd4dd48b66ac658bf0
Hritik Pandey <70340237+HritikPandey22@users.noreply.github.com>
2022-06-30 14:04:38 +0530

    Add files via upload

---
+++ Sieve of Erathosthenes/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Seive from './seive';
+// import Controller from './Controller/controller';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Controller/> */}
+      <Seive/>
+    </div>
+  );
+}
+
+export default App;
